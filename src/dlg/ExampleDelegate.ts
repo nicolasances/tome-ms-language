7f5f0534bca149af12177806c9009a94a5a90e93
nicolasances <nicolas.matteazzi@gmail.com>
2026-04-25 15:32:21 +0200

    Fixed example delegate

--- src/dlg/ExampleDelegate.ts
+++ src/dlg/ExampleDelegate.ts
@@ -1,7 +1,11 @@
 import { Request } from "express";
 import { TotoDelegate, UserContext } from "totoms";
 
-export class SayHello extends TotoDelegate {
+export class SayHello extends TotoDelegate<any, any> {
+    
+    parseRequest(req: Request) {
+        return {}
+    }
 
     async do(req: Request, userContext?: UserContext): Promise<any> {
         return { message: "Hello World!" };
